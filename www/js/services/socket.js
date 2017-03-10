0d8dac4afb6fdc3854632377077054981b177b95
webstar1987923 <webstar1987923@hotmail.com>
2017-03-10 15:00:17 +0300

    Update socket.js

--- www/js/services/socket.js
+++ www/js/services/socket.js
@@ -1,7 +1,8 @@
 app.factory('socket',function(socketFactory){
 	//Create socket and connect to http://chat.socket.io 
    //var socket = io.connect('http://192.168.1.118:3001/');
-    var socket = io.connect('http://162.243.225.225:3002');
+    //var socket = io.connect('http://162.243.225.225:3002');
+  var socket = io.connect('http://138.197.125.16:3002');
    
   	mySocket = socketFactory({
     	ioSocket: socket
